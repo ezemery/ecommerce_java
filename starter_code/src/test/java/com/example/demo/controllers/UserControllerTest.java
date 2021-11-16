5b6b999b97f29eff2b109f700988a6d68f5e32b8
Ezechukwu Emmanuel <emereuwaonueze@gmail.com>
2021-11-16 22:22:53 +0100

    add controller tests

---
+++ starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+    private UserController userController;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository= mock(CartRepository.class);
+    private BCryptPasswordEncoder bCryptPasswordEncoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setup(){
+        userController = new UserController();
+        TestUtils.injectObject(userController, "userRepository", userRepository);
+        TestUtils.injectObject(userController, "cartRepository",cartRepository);
+        TestUtils.injectObject(userController,"bCryptPasswordEncoder", bCryptPasswordEncoder);
+    }
+
+    @Test
+    public void testCreateUser(){
+        when(bCryptPasswordEncoder.encode("testPassword")).thenReturn("Hello World");
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("test");
+        createUserRequest.setPassword("testPassword");
+        createUserRequest.setConfirmPassword("testPassword");
+
+        ResponseEntity<User> user = userController.createUser(createUserRequest);
+        assertNotNull(user);
+        assertEquals(200, user.getStatusCodeValue());
+
+        User u = user.getBody();
+        assertNotNull(u);
+        assertEquals("test",u.getUsername());
+        assertEquals("Hello World", u.getPassword());
+    }
+}
